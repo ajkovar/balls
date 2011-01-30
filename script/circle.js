41e18d1b5ba13fe71a16a00737f0d381cc0d1c11
Alex Kovar <ajkovar@gmail.com>
2011-01-29 21:30:21 -0600

    reformat

--- script/circle.js
+++ script/circle.js
@@ -1,82 +1,82 @@
 (function($){
-		
-	var Circle = window.Circle = function(ctx, radius, canvasWidth, canvasHeight){
-	    this.ctx = ctx;
-	    this.radius = radius;
-            this.canvasWidth = canvasWidth;
-            this.canvasHeight = canvasHeight;
+    
+    var Circle = window.Circle = function(ctx, radius, canvasWidth, canvasHeight){
+	this.ctx = ctx;
+	this.radius = radius;
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+    }
+
+    Circle.prototype = {
+        calculateVelocity: function() {
+            
+            var x = this.x,
+            y = this.y,
+            dx = this.dx,
+            dy = this.dy;
+
+	    if(x<=10 || x>=this.canvasWidth-10){
+		dx = -dx/2;
+	    }
+
+	    if(y<=10 || y>=this.canvasHeight-10){
+		dy = -dy/2;
+	    }
+	    
+	    dx=dx/1.02
+	    
+	    if(Math.abs(dx)<.2)
+		dx=0
+	    
+	    dx+=this.ax
+	    dy+=this.ay
+
+            this.dx = dx;
+            this.dy = dy;
+        },
+        calculatePosition: function() {
+            var x = this.x;
+            var y = this.y
+            
+	    x+=this.dx;
+	    y+=this.dy;
+	    
+	    if(x<10)
+		x=10;
+	    else if(x>this.canvasWidth-10)
+		x=this.canvasWidth-10
+	    
+	    if(y<10)
+		y=10
+	    else if(y>this.canvasHeight-10)
+		y=this.canvasHeight-10
+
+            this.x=x;
+            this.y=y;
+        },
+	draw: function(x,y) {
+	    var ctx = this.ctx;
+	    ctx.beginPath();
+	    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+	    ctx.closePath();
+	    ctx.fill();
 	}
-
-	Circle.prototype = {
-            calculateVelocity: function() {
-                
-                var x = this.x,
-                y = this.y,
-                dx = this.dx,
-                dy = this.dy;
-
-		if(x<=10 || x>=this.canvasWidth-10){
-		    dx = -dx/2;
-		}
-
-		if(y<=10 || y>=this.canvasHeight-10){
-		    dy = -dy/2;
-		}
-		
-		dx=dx/1.02
-		
-		if(Math.abs(dx)<.2)
-		    dx=0
-		
-		dx+=this.ax
-		dy+=this.ay
-
-                this.dx = dx;
-                this.dy = dy;
-            },
-            calculatePosition: function() {
-                var x = this.x;
-                var y = this.y
-                
-		x+=this.dx;
-		y+=this.dy;
-		
-		if(x<10)
-		    x=10;
-		else if(x>this.canvasWidth-10)
-		    x=this.canvasWidth-10
-		
-		if(y<10)
-		    y=10
-		else if(y>this.canvasHeight-10)
-		    y=this.canvasHeight-10
-
-                this.x=x;
-                this.y=y;
-            },
-		draw: function(x,y) {
-		  var ctx = this.ctx;
-		  ctx.beginPath();
-		  ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
-		  ctx.closePath();
-		  ctx.fill();
-		}
+    }
+    
+    var Rectangle = window.Rectangle = function(ctx, width, height){
+	this.ctx = ctx
+	this.width = width
+	this.height = height
+    }
+    
+    Rectangle.prototype = {
+	draw: function(x,y) {
+	    this.ctx.beginPath();
+	    this.ctx.rect(x,y,this.width,this.height);
+	    this.ctx.closePath();
+	    this.ctx.fill();
 	}
-	
-	var Rectangle = window.Rectangle = function(ctx, width, height){
-		this.ctx = ctx
-		this.width = width
-		this.height = height
-	}
-	
-	Rectangle.prototype = {
-		draw: function(x,y) {
-		  this.ctx.beginPath();
-		  this.ctx.rect(x,y,this.width,this.height);
-		  this.ctx.closePath();
-		  this.ctx.fill();
-		}
-	}
-	
+    }
+    
 }(jQuery));
 
--- script/test.js
+++ script/test.js
@@ -1,76 +1,76 @@
 (function($){
-	$(document).ready(function(){
-		
-		var canvasHeight = $(window).height() - $("#header").outerHeight() -30,
-			canvasWidth = $(window).width();
-		
-		$('body').append("<canvas id=\"canvas\" width='" + canvasWidth + "' height='" + canvasHeight + "'></canvas>")
-		
-		console.log(canvasHeight)
-		console.log(canvasWidth)
-		
-
-
-		var canvas = $('#canvas'),
-	    ctx = canvas[0].getContext("2d");
-
-		var circle = new Circle(ctx, 10, canvasWidth, canvasHeight)
-
-	    circle.x=10,
-	    circle.y=10,
-	    circle.dx=Math.floor(Math.random()*11)*2,
-	    circle.dy=Math.floor(Math.random()*11)*2,
-	    circle.ax=0,
-	    circle.ay=.5
-		
-		
-		setInterval(function(){
-			
-			ctx.clearRect(0,0,canvasWidth,canvasHeight);
-			
-                    circle.calculateVelocity();
-                    circle.calculatePosition();
-		    circle.draw();
-			
-			
-		}, 25)
-		
-		
-		var keypressHandler = new KeypressHandler(document)
-		
-		//left
-		keypressHandler.press(37, function(){
-			circle.ax-=1
-		})
-		keypressHandler.up(37, function(){
-			circle.ax+=1
-		})
-		
-		//up
-		keypressHandler.press(38, function(){
-			circle.ay-=1
-		})
-		keypressHandler.up(38, function(){
-			circle.ay+=1
-		})
-		
-		//right
-		keypressHandler.press(39, function(){
-			circle.ax+=1
-		})
-		keypressHandler.up(39, function(){
-			circle.ax-=1
-		})
-		
-		//down
-		keypressHandler.press(40, function(){
-			circle.ay+=1
-		})
-		keypressHandler.up(40, function(){
-			circle.ay-=1
-		})
+    $(document).ready(function(){
 	
+	var canvasHeight = $(window).height() - $("#header").outerHeight() -30,
+	canvasWidth = $(window).width();
+	
+	$('body').append("<canvas id=\"canvas\" width='" + canvasWidth + "' height='" + canvasHeight + "'></canvas>")
+	
+	console.log(canvasHeight)
+	console.log(canvasWidth)
+	
+
+
+	var canvas = $('#canvas'),
+	ctx = canvas[0].getContext("2d"),
+
+	circle = new Circle(ctx, 10, canvasWidth, canvasHeight)
+
+	circle.x=10,
+	circle.y=10,
+	circle.dx=Math.floor(Math.random()*11)*2,
+	circle.dy=Math.floor(Math.random()*11)*2,
+	circle.ax=0,
+	circle.ay=.5
+	
+	
+	setInterval(function(){
+	    
+	    ctx.clearRect(0,0,canvasWidth,canvasHeight);
+	    
+            circle.calculateVelocity();
+            circle.calculatePosition();
+	    circle.draw();
+	    
+	    
+	}, 25)
+	
+	
+	var keypressHandler = new KeypressHandler(document)
+	
+	//left
+	keypressHandler.press(37, function(){
+	    circle.ax-=1
+	})
+	keypressHandler.up(37, function(){
+	    circle.ax+=1
 	})
 	
+	//up
+	keypressHandler.press(38, function(){
+	    circle.ay-=1
+	})
+	keypressHandler.up(38, function(){
+	    circle.ay+=1
+	})
+	
+	//right
+	keypressHandler.press(39, function(){
+	    circle.ax+=1
+	})
+	keypressHandler.up(39, function(){
+	    circle.ax-=1
+	})
+	
+	//down
+	keypressHandler.press(40, function(){
+	    circle.ay+=1
+	})
+	keypressHandler.up(40, function(){
+	    circle.ay-=1
+	})
+	
+    })
+    
 }(jQuery));
 
