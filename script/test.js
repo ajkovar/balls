f9e58631813dae80db642480eea17734e535dfc2
Alex Kovar <ajkovar@gmail.com>
2011-01-29 21:29:32 -0600

    put calculations into circle object

--- script/test.js
+++ script/test.js
@@ -9,51 +9,29 @@
 		console.log(canvasHeight)
 		console.log(canvasWidth)
 		
+
+
 		var canvas = $('#canvas'),
-			ctx = canvas[0].getContext("2d"),
-			x=10,
-			y=10,
-			dx=Math.floor(Math.random()*11)*2,
-			dy=Math.floor(Math.random()*11)*2,
-			ax=0,
-			ay=.5
+	    ctx = canvas[0].getContext("2d");
+
+		var circle = new Circle(ctx, 10, canvasWidth, canvasHeight)
+
+	    circle.x=10,
+	    circle.y=10,
+	    circle.dx=Math.floor(Math.random()*11)*2,
+	    circle.dy=Math.floor(Math.random()*11)*2,
+	    circle.ax=0,
+	    circle.ay=.5
 		
-		var circle = new Circle(ctx, 10)
 		
 		setInterval(function(){
 			
 			ctx.clearRect(0,0,canvasWidth,canvasHeight);
 			
-			x+=dx;
-			y+=dy;
+                    circle.calculateVelocity();
+                    circle.calculatePosition();
+		    circle.draw();
 			
-			if(x<10)
-				x=10;
-			else if(x>canvasWidth-10)
-				x=canvasWidth-10
-			
-			if(y<10)
-				y=10
-			else if(y>canvasHeight-10)
-				y=canvasHeight-10
-			
-			circle.draw(x, y)
-			
-			if(x<=10 || x>=canvasWidth-10){
-				dx = -dx/2;
-			}
-
-			if(y<=10 || y>=canvasHeight-10){
-				dy = -dy/2;
-			}
-			
-			dx=dx/1.02
-			
-			if(Math.abs(dx)<.2)
-				dx=0
-			
-			dx+=ax
-			dy+=ay
 			
 		}, 25)
 		
@@ -62,34 +40,34 @@
 		
 		//left
 		keypressHandler.press(37, function(){
-			ax-=1
+			circle.ax-=1
 		})
 		keypressHandler.up(37, function(){
-			ax+=1
+			circle.ax+=1
 		})
 		
 		//up
 		keypressHandler.press(38, function(){
-			ay-=1
+			circle.ay-=1
 		})
 		keypressHandler.up(38, function(){
-			ay+=1
+			circle.ay+=1
 		})
 		
 		//right
 		keypressHandler.press(39, function(){
-			ax+=1
+			circle.ax+=1
 		})
 		keypressHandler.up(39, function(){
-			ax-=1
+			circle.ax-=1
 		})
 		
 		//down
 		keypressHandler.press(40, function(){
-			ay+=1
+			circle.ay+=1
 		})
 		keypressHandler.up(40, function(){
-			ay-=1
+			circle.ay-=1
 		})
 	
 	})
